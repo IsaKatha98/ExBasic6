e6045705cf9fb7abe130f9e505d9652600f4903a
ikloerzer <ikloerzer@106-14.info2>
2022-09-26 08:43:38 +0200

    C1

---
+++ ExBasic6/src/exbasic6/ExBasic6.java
@@ -0,0 +1,27 @@
+package exbasic6;
+
+public class ExBasic6 {
+
+	public static void main(String[] args) {
+	
+		//Creo el escáner
+		Scanner sc= new Scanner (System.in); 
+		
+		//Declaro las variables
+		
+		double num1;
+		double num2;
+		double sum;
+		double resta;
+		double mult;
+		double div;
+		
+		//Le pido al usuario que introduzca los números y los guardo
+		System.out.print("Introduzca el primer número y pulse Intro: ");
+		num1 =sc.nextDouble();
+		System.out.println();
+		System.out.print("Introduzca el segundo número y pulse Intro: ");
+
+	}
+
+}
